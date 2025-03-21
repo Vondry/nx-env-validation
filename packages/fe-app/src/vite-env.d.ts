6cdbd127cb8eedf521b45cfaacd6c91f1aaeee66
Tomas Vondracek <tom@Tomass-MacBook-Pro.local>
2025-03-21 16:22:00 +0100

    fix: env type definition

--- packages/fe-app/src/vite-env.d.ts
+++ packages/fe-app/src/vite-env.d.ts
@@ -5,4 +5,8 @@ type ImportMetaEnvAugmented = import("@julr/vite-plugin-validate-env").ImportMet
 >
 
 // https://github.com/Julien-R44/vite-plugin-validate-env?tab=readme-ov-file#typing-importmetaenv
-type ImportMetaEnv = ImportMetaEnvAugmented
+// eslint-disable-next-line
+interface ImportMetaEnv extends ImportMetaEnvAugmented {
+    // Now import.meta.env is totally type-safe and based on your `env.ts` schema definition
+    // You can also add custom variables that are not defined in your schema
+}
